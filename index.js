00b87328cfff8059748a8ac223299b6436c17a8d
PRATHA BAGTHARIA <124965525+Pratha3@users.noreply.github.com>
2024-06-10 09:56:10 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./router/user.router');
 const connectDB = require('./config/database');
-connectDB();
 
-const app = express();
+(async () => {
+    await connectDB();
 
-const port = 8888;
+    const app = express();
+    const port = 8888;
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.set('view engine', 'ejs');
+    app.use("/uploads", express.static('uploads'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.set('view engine', 'ejs');
-app.use("/uploads", express.static('uploads'));
+    app.use(router);
 
-app.use(router)
-
-app.listen(port, (err) => {
-    console.log(`server is running on port http://localhost:${port}`);
-});
+    app.listen(port, (err) => {
+        if (err) {
+            console.error('Error starting server:', err);
+        } else {
+            console.log(`Server is running on port http://localhost:${port}`);
+        }
+    });
+})();
